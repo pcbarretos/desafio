51991de2b56c670490d91e7811fceb591124f59f
Paulo Barreto <pcbarreto@minsait.com>
2021-12-24 03:48:40 -0300

    ajuste entrypoint

--- data.sh
+++ data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sudo yum install -y yum-utils device-mapper-persistent-data lvm2
+sudo yum install -y yum-utils device-mapper-persistent-data lvm2 curl
 sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 sudo yum install docker-ce
 sudo systemctl start docker
-sudo docker run --rm -p 80:80 nginx
+docker run --rm -p 80:8080 bitnami/wordpress-nginx
